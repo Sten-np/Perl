9a4535de4df85bec1efc2c2f151ef26ba051ff87
Sten <146124490+Sten-np@users.noreply.github.com>
2024-12-03 22:11:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+
+# Perl 
+
+# Scalar: Single value
+$number = 42;    # A number
+$string = "Hi!"; # A string
+
+print "$number\n + $string\n";
+
+# Arrays: Like lists in other languages
+@fruits = ("apple", "banana", "cherry");
+print $fruits[0]; # Outputs "apple"
+
+# Hashes: Like Python dictionaries
+%colors = ("red" => "#FF0000", "green" => "#00FF00");
+print $colors{"red"}; # Outputs "#FF0000"
